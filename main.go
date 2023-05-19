150fff701aa28e5147918f1320888721b36cb704
Jonas Vadebo <jonas.vadebo@king.com>
2023-05-19 11:39:41 +0200

    v7.7.0

--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
+
 func main() {
+	//Create instances of struct
+	u1 := User{
+		ID:        1,
+		FirstName: "Jonas",
+		LastName:  "Vadebo",
+	}
+	u2 := User{
+		ID:        2,
+		FirstName: "Negin",
+		LastName:  "Moradi",
+	}
+	if u1.ID == u2.ID {
+		println("Same user")
+	}
 
-	println("Starting Web server")
-	//Panic and branching
-	panic("Something bad happened")
+	if u1.ID != u2.ID {
+		println("Not the same user!")
+	}
 
-	println("Web server started")
+	if u1.ID == u2.ID {
+		println("Same user")
+	} else {
+		println("Not the same user!")
+	}
 
+	if u1.ID == u2.ID {
+		println("Same user")
+	} else if u1.FirstName == u2.FirstName {
+		println("Similar user!")
+	} else {
+		println("Diferent user!")
+	}
+
+	if u1 == u2 {
+		println("Same user")
+	} else if u1.FirstName == u2.FirstName {
+		println("Similar user!")
+	}
 }
