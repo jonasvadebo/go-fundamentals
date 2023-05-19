d97c14a584750ade506c6678a9e7fd33e139365b
Jonas Vadebo <jonas.vadebo@king.com>
2023-05-19 11:03:46 +0200

    v7.6.0

--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
 func main() {
-	//Types of loops
-	//All loops will be for-loops
 
-	//1) Loop until condition is met
-	//2) loop until condition with post clause
-	//3) infinite loops
-	//4) loop over collections.
+	println("Starting Web server")
+	//Panic and branching
+	panic("Something bad happened")
 
-	/** loop over collections **/
-	slice := []int{1, 2, 3, 4, 5}
-
-	for i := 0; i < len(slice); i++ {
-		println(slice[i])
-	}
-
-	for i, v := range slice {
-		println(i, v)
-	}
-
-	wellKnownPorts := map[string]int{"http": 80, "https": 443}
-
-	for _, v := range wellKnownPorts {
-		println(v)
-	}
-
-	for k, v := range wellKnownPorts {
-		println(k, v)
-	}
+	println("Web server started")
 
 }
